af222f7d54fcafabc541fe49bc82adb110e4e2bf
Hrodvitnir-Fenrir <44929201+Hrodvitnir-Fenrir@users.noreply.github.com>
2021-11-22 20:12:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv/config");
+const Discord = require("discord.js");
+const intents = new Discord.Intents(32767);
+const client = new Discord.Client({ intents });
+const { MessageEmbed, MessageActionRow, MessageSelectMenu, MessageButton, Message, Permissions } = require('discord.js');
+const VirusTotalApi = require("virustotal-api");
+const virusTotal = new VirusTotalApi(process.env.API_KEY);
+
+client.once("ready", async () => {
+    console.log("En ligne !");
+    client.user.setPresence({ status: 'dnd', activities: [{ type: "WATCHING", name: "des liens d'arnaque" }] });
+
+});
+
+let urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+
+client.on("messageCreate", async (message) => {
+    if (message.content.includes("https://discord.gg/" || "http://discord.gg/" || "https://twitter.com/" || "https://www.youtube.com/" || "https://twitch.tv/")) {
+        return;
+    }
+    if (message.content.match(urlRegex)) {
+        console.log(message.content.match(urlRegex))
+        let link = message.content.match(urlRegex).toString()
+        console.log(link)
+        virusTotal.urlScan(link).then(response => {
+            let resource = response.resource;
+            virusTotal.urlReport(resource).then(result => {
+                console.log(result.positives);
+            })
+        });
+    }
+})
+
+client.login(process.env.DISCORD_TOCKEN);
